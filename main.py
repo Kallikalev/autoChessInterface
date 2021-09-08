131054a9b4f9316fd5b6dfbeb64f4b6d1069d42c
Kalev <kallikalev@gmail.com>
2021-09-08 10:58:55 -0400

    exportFEN function implemented

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 gameState = {
-  board = [], # array of arrays, (0,0) is top left, (7,7) is bottom right
-  nextMove = "w",
-  castling = "KQkq", # k means kingside, q means queenside, uppercase is white and lowercase is black, a dash indicates no castling available
-  passant = "-", # the target square in algabreic notation, this is the square "behind" the pawn
-  halfClock = 0, # the number of moves since the last capture or pawn move, used for the fifty-move tie rule
-  fullmoveNum = 1 # the number of the full move, incremented after black's move
+  "board" : [], # array of arrays, [0][0] is top left, [7][7] is bottom right
+  "nextMove" : "w",
+  "castling" : "KQkq", # k means kingside, q means queenside, uppercase is white and lowercase is black, a dash indicates no castling available
+  "passant" : "-", # the target square in algabreic notation, this is the square "behind" the pawn
+  "halfClock" : 0, # the number of moves since the last capture or pawn move, used for the fifty-move tie rule
+  "fullmoveNum" : 1 # the number of the full move, incremented after black's move
 }
 
 # piece representations (white is uppercase, black is lowercase):
@@ -16,7 +16,7 @@ gameState = {
 # queen: Q
 # king: K
 
-def importFEN(fenString):
+def importFEN(fenString): # turn a FEN string into a game state dictionary
   # split the FEN string into it's individual pieces
   segments = fenString.split(" ")
   ranks = segments[0].split("/")
@@ -30,10 +30,32 @@ def importFEN(fenString):
           newBoard[rank].append("-")
       else:
         newBoard[rank].append(c)
-  return {board = newBoard, nextMove = segments[1], castling = segments[2], passant = segments[3], halfClock = segments[4], fullmoveNum = segments[5]}
+  return {"board" : newBoard, "nextMove" : segments[1], "castling" : segments[2], "passant" : segments[3], "halfClock" : int(segments[4]), "fullmoveNum" : int(segments[5])}
+
+def exportFEN(gameState): #turn a game state dictionary into a FEN string
+  boardString = ""
+  for rank in gameState["board"]:
+    numEmpty = 0 # count number of empty spaces in a row
+    for file in rank:
+      if (file == "-"):
+        numEmpty = numEmpty + 1
+      else:
+        if (numEmpty != 0):
+          boardString = boardString + str(numEmpty)
+          numEmpty = 0
+        boardString = boardString + file
+    if (numEmpty != 0):
+      boardString = boardString + str(numEmpty)
+    boardString = boardString + "/"
+  boardString = boardString[0:-1] # remove last slash using slicing
+  
+  return boardString + " " + gameState["nextMove"] + " " + gameState["castling"] + " " + gameState["passant"] + " " + str(gameState["halfClock"]) + " " + str(gameState["fullmoveNum"])
+      
 
 print(gameState)
 
 gameState = importFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 
 print(gameState)
+
+print(exportFEN(gameState))
